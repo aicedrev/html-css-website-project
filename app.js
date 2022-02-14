2afc201851aed4c923ed552ef889592ba12d3f14
aicedrev <josephverdecia@gmail.com>
2022-02-14 13:40:23 -0800

    Project update

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const menuLinks = document.querySelector('.navbar__menu')
 
 
 menu.addEventListener('click', function() {
-	menu.classLast.toggle('is-active')
+	menu.classList.toggle('is-active')
 	menuLinks.classList.toggle('active');
 })
